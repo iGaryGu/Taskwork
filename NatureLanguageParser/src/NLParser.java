361b692471431e9830dd7cc3887e0dba32cf6fd8
damonshen <thanatos1710@hotmail.com>
2013-12-02 23:39:54 +0800

    create project

---
+++ NatureLanguageParser/src/NLParser.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+
+public class NLParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//return the TF-IDF of all word
+	public static HashMap<String, Double> getTFIDF(String cmd){
+		return null;
+	}
+}
